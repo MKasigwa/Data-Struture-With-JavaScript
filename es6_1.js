d14c69abce071fcd811a24581859383712d46272
Miracle Kasigwa <miraclekasigwa@gmail.com>
2021-10-19 13:41:10 +0200

    Added Es6 lecon 1

---
+++ es6_1.js
@@ -0,0 +1,35 @@
+// Explore Differences Between the var and let KeywordsPassed
+
+/*
+One of the biggest problems with declaring variables with the var keyword is that you can 
+overwrite variable declarations without an error.
+*/
+
+var camper = 'James';
+var camper = 'David';
+console.log(camper);
+
+//Here the console will display the string David.
+
+/*
+As you can see in the code above, the camper variable is originally declared as James and then overridden to be David. 
+In a small application, you might not run into this type of problem, but when your code becomes larger, 
+you might accidentally overwrite a variable that you did not intend to overwrite. 
+Because this behavior does not throw an error, searching and fixing bugs becomes more difficult.
+A new keyword called let was introduced in ES6 to solve this potential issue with the var keyword. 
+If you were to replace var with let in the variable declarations of the code above, the result would be an error.
+*/
+
+let camper = 'James';
+let camper = 'David';
+
+/*
+This error can be seen in the console of your browser. So unlike var, when using let, a variable with the same name 
+can only be declared once. Note the "use strict". This enables Strict Mode, which catches common coding 
+mistakes and "unsafe" actions. For instance:
+*/
+
+"use strict";
+x = 3.14;
+
+//This will display an error that x is not defined.
