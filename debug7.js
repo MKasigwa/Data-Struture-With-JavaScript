265d00634283f30fa39c93ae0626d730a3eca8de
Miracle Kasigwa <miraclekasigwa@gmail.com>
2021-10-19 08:27:07 +0200

    Added quiz for lecon 7

--- debug7.js
+++ debug7.js
@@ -15,3 +15,22 @@ let varOne = myFunction;
 let varTwo = myFunction();
 
 //Here varOne is the function myFunction, and varTwo is the string You rock!.
+
+//QUIZ
+//=====
+
+
+// Fix the code so the variable result is set to the value returned from calling the function getNine.
+function getNine() {
+    let x = 6;
+    let y = 3;
+    return x + y;
+  }
+  
+  let result = getNine;
+  console.log(result);
+
+  //Answer
+  
+result = getNine();
+  console.log(result);
