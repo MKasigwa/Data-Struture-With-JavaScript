5340eb44f4173720cf6c5f7c3723db96c33ab6ce
Miracle Kasigwa <miraclekasigwa@gmail.com>
2021-10-19 08:38:24 +0200

    Added and solved lecon 9

--- debug9.js
+++ debug9.js
@@ -24,3 +24,31 @@ for (let k = 0; k < len; k++) {
 
 //The first example here loops one too many times, and the second loops one too few times (missing the first index, 0). The third example is correct.
 
+//QUIZ
+//====
+
+//Fix the two indexing errors in the following function so all the numbers 1 through 5 are printed to the console.
+function countToFive() {
+    let firstFive = "12345";
+    let len = firstFive.length;
+    // Only change code below this line
+    for (let i = 1; i <= len; i++) {
+    // Only change code above this line
+      console.log(firstFive[i]);
+    }
+  }
+  
+  countToFive();
+
+  //ANSWER
+  function countToFive() {
+    let firstFive = "12345";
+    let len = firstFive.length;
+    // Only change code below this line
+    for (let i = 0; i < len; i++) {
+    // Only change code above this line
+      console.log(firstFive[i]);
+    }
+  }
+  
+  countToFive();
