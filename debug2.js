961f7d78bf4b03ab9dd514f66126af53a3dcc576
Miracle Kasigwa <miraclekasigwa@gmail.com>
2021-10-19 07:37:04 +0200

    Implemented QUIZ for the second lecon

--- debug2.js
+++ debug2.js
@@ -18,3 +18,18 @@ console.log(typeof {});
 
 // JavaScript recognizes six primitive (immutable) data types: Boolean, Null, Undefined, Number, String, and Symbol (new with ES6) and one type for mutable items: Object. 
 // Note that in JavaScript, arrays are technically a type of object.
+
+
+//QUIZ
+//=========
+
+
+//Add two console.log() statements to check the typeof each of the two variables seven and three in the code.
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+// Only change code below this line
+
+//Answer
+console.log(typeof seven);
+console.log(typeof three);
