27f97f4113c2d386bfeedfd16db5639f901c1705
Miracle Kasigwa <miraclekasigwa@gmail.com>
2021-10-21 12:18:07 +0200

    Added and Solved ES6 QUIZ 8

--- es6_8.js
+++ es6_8.js
@@ -20,3 +20,26 @@ As you can see in the example above, the parameter name will receive its default
 when you do not provide a value for the parameter. You can add default values for as many 
 parameters as you want.
 */
+
+//QUIZ
+//========
+
+/*
+Modify the function increment by adding default parameters so that it will add 1 to number if value 
+is not specified.
+*/
+
+// Only change code below this line
+const increment = (number, value) => number + value;
+// Only change code above this line
+
+
+//ANSWER
+//======
+
+// Only change code below this line
+const increment = (number, value=1) => number + value;
+// Only change code above this line
+
+
+
