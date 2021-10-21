8f30dd4979609aeb2981db2b90d9f9fabf32837f
Miracle Kasigwa <miraclekasigwa@gmail.com>
2021-10-21 11:52:45 +0200

    Added and solved ES6 QUIZ 4

--- es6_4.js
+++ es6_4.js
@@ -27,4 +27,36 @@ As you can see, you can mutate the object [5, 6, 7] itself and the variable s wi
 altered array [5, 6, 45]. Like all arrays, the array elements in s are mutable, but because 
 const was used, you cannot use the variable identifier s to point to a different array 
 using the assignment operator.
-/*
+*/
+
+//QUIZ
+//=====
+
+
+//An array is declared as const s = [5, 7, 2]. Change the array to [2, 5, 7] using various element 
+assignments.
+
+const s = [5, 7, 2];
+function editInPlace() {
+  // Only change code below this line
+
+  // Using s = [2, 5, 7] would be invalid
+
+  // Only change code above this line
+}
+editInPlace();
+
+
+
+//ANSWER
+const s = [5, 7, 2];
+function editInPlace() {
+  // Only change code below this line
+
+  // Using s = [2, 5, 7] would be invalid
+  s[0]=2;
+  s[1]=5;
+  s[2]=7;
+  // Only change code above this line
+}
+editInPlace();
