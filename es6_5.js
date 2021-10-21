e9b51792d11a6482a7c932262a2c4357275c5096
Miracle Kasigwa <miraclekasigwa@gmail.com>
2021-10-21 11:58:03 +0200

    Added and Solved ES6 QUIZ 5

--- es6_5.js
+++ es6_5.js
@@ -22,3 +22,49 @@ console.log(obj);
 The obj.review and obj.newProp assignments will result in errors, because our editor runs in strict 
 mode by default, and the console will display the value { name: "FreeCodeCamp", review: "Awesome" }.
 */
+
+//QUIZ
+//=====
+
+/*
+In this challenge you are going to use Object.freeze to prevent mathematical constants from changing. 
+You need to freeze the MATH_CONSTANTS object so that no one is able to alter the value of PI, 
+add, or delete properties.
+*/
+
+function freezeObj() {
+    const MATH_CONSTANTS = {
+      PI: 3.14
+    };
+    // Only change code below this line
+  
+  
+    // Only change code above this line
+    try {
+      MATH_CONSTANTS.PI = 99;
+    } catch(ex) {
+      console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
+  }
+  const PI = freezeObj();
+
+  //ANSWER
+  //=======
+
+  function freezeObj() {
+    const MATH_CONSTANTS = {
+      PI: 3.14
+    };
+    // Only change code below this line
+  Object.freeze(MATH_CONSTANTS);
+  
+    // Only change code above this line
+    try {
+      MATH_CONSTANTS.PI = 99;
+    } catch(ex) {
+      console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
+  }
+  const PI = freezeObj();
