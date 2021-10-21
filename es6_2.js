a24815607a67d85d1c31b3e1d414bdd08fb91d86
Miracle Kasigwa <miraclekasigwa@gmail.com>
2021-10-21 11:22:09 +0200

    Added and solved ES6 QUIZ 2

--- es6_2.js
+++ es6_2.js
@@ -81,3 +81,38 @@ loop statement. printNumTwo() returned the correct value because three different
 with unique values (0, 1, and 2) were created by the let keyword within the loop statement.
 
 */
+
+
+//QUIZ
+
+/*
+Fix the code so that i declared in the if statement is a separate variable than i declared in the first line of the function. Be certain not to use the var keyword anywhere in your code.
+
+This exercise is designed to illustrate the difference between how var and let keywords assign scope to 
+the declared variable. When programming a function similar to the one used in this exercise, 
+it is often better to use different variable names to avoid confusion.
+
+*/
+
+function checkScope() {
+    var i = 'function scope';
+    if (true) {
+      i = 'block scope';
+      console.log('Block scope i is: ', i);
+    }
+    console.log('Function scope i is: ', i);
+    return i;
+  }
+
+
+  //ANSWER
+
+  function checkScope() {
+    let i = 'function scope';
+    if (true) {
+      let i = 'block scope';
+      console.log('Block scope i is: ', i);
+    }
+    console.log('Function scope i is: ', i);
+    return i;
+  }
