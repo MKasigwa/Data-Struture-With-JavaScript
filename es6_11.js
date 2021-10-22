232f11399eeb8c9ee3ea9a620c700c8ca254e202
Miracle Kasigwa <miraclekasigwa@gmail.com>
2021-10-22 09:26:12 +0200

    Added and Solved ES6 QUIZ 11

--- es6_11.js
+++ es6_11.js
@@ -33,3 +33,40 @@ Here, the name and age variables will be created and assigned the values of thei
 
 You can extract as many or few values from the object as you want.
 */
+
+//QUIZ
+//=====
+
+/*
+Replace the two assignments with an equivalent destructuring assignment. 
+It should still assign the variables today and tomorrow the values of 
+today and tomorrow from the HIGH_TEMPERATURES object.
+*/
+
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+  };
+  
+  // Only change code below this line
+  
+  const today = HIGH_TEMPERATURES.today;
+  const tomorrow = HIGH_TEMPERATURES.tomorrow;
+  
+  // Only change code above this line
+
+
+  //ANSWER
+  //======
+  const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+  };
+  
+  // Only change code below this line
+  
+  const {today,tomorrow} = HIGH_TEMPERATURES;
+  
+  // Only change code above this line
