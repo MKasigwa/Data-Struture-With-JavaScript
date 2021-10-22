2b1b83d6f52ecfde95253375fdda554c9b190d55
Miracle Kasigwa <miraclekasigwa@gmail.com>
2021-10-22 09:41:21 +0200

    Added and Solved ES6 QUIZ 13

--- es6_13.js
+++ es6_13.js
@@ -25,3 +25,20 @@ const [a, b,,, c] = [1, 2, 3, 4, 5, 6];
 console.log(a, b, c);
 
 //The console will display the values of a, b, and c as 1, 2, 5.
+
+//QUIZ
+//====
+
+/*
+Use destructuring assignment to swap the values of a and b so that a receives 
+the value stored in b, and b receives the value stored in a.
+*/
+
+let a = 8, b = 6;
+// Only change code below this line
+
+//ANSWER
+//======
+let a = 8, b = 6;
+// Only change code below this line
+[a,b]=[b,a];
