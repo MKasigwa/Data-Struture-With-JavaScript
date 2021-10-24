612b56138f9f98eb59c5770f43b1481a506dd11c
Miracle Kasigwa <miraclekasigwa@gmail.com>
2021-10-24 09:54:27 +0200

    Added and Solved BDS Quiz 3

--- basicDS3.js
+++ basicDS3.js
@@ -29,3 +29,34 @@ romanNumerals would have the value ['XIX', 'XX', 'XXI', 'XXII', 'XXIII'].
  Notice that we can also pass variables, which allows us even greater 
  flexibility in dynamically modifying our array's data.
  */
+
+ //QUIZ
+ //=====
+
+ /*
+ We have defined a function, mixedNumbers, which we are passing an array as an argument.
+  Modify the function by using push() and unshift() to add 'I', 2, 'three' to 
+  the beginning of the array and 7, 'VIII', 9 to the end so that the returned
+   array contains representations of the numbers 1-9 in order.
+   */
+   function mixedNumbers(arr) {
+    // Only change code below this line
+  
+    // Only change code above this line
+    return arr;
+  }
+  
+  console.log(mixedNumbers(['IV', 5, 'six']));
+
+
+  //ANSWER
+  //======
+
+  function mixedNumbers(arr) {
+    // Only change code below this line
+    arr.unshift('I',2,'three');
+  arr.push(7,"VIII",9);
+    // Only change code above this line
+    return arr;
+  }
+  console.log(mixedNumbers(['IV', 5, 'six']));
