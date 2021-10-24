02cf1a395943ab1c47771268d30fd45b40464597
Miracle Kasigwa <miraclekasigwa@gmail.com>
2021-10-24 09:48:45 +0200

    Added and solved BDS QUIZ 2

--- basicDS2.js
+++ basicDS2.js
@@ -40,3 +40,26 @@ ourArray[1] = "not b anymore";
 Using bracket notation, we have now reset the item at index 1 from the string b, 
 to not b anymore. Now ourArray is ["a", "not b anymore", "c"].
 */
+
+//QUIZ
+//=====
+
+/*In order to complete this challenge, set the 2nd position (index 1) 
+of myArray to anything you want, besides the letter b.
+*/
+
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+
+// Only change code above this line
+console.log(myArray);
+
+
+//ANSWER
+//=======
+
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[1]="z";
+// Only change code above this line
+console.log(myArray);
