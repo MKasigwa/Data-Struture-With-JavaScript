11dd0f4deb4c1bcaf953ad003d288af3aac3c06b
Miracle Kasigwa <miraclekasigwa@gmail.com>
2021-10-25 15:11:14 +0200

    Added and Solved BDS QUIZ 6

--- basicDS6.js
+++ basicDS6.js
@@ -25,3 +25,32 @@ to be deleted (1), and the remaining arguments (13, 14) will be inserted
 starting at that same index. Note that there can be any number of elements 
 (separated by commas) following amountToDelete, each of which gets inserted.
 */
+
+
+//QUIZ
+
+/*We have defined a function, htmlColorNames, which takes an array of HTML 
+colors as an argument. Modify the function using splice() to remove the 
+first two elements of the array and add 'DarkSalmon' and 'BlanchedAlmond' in 
+their respective places.
+*/
+
+function htmlColorNames(arr) {
+    // Only change code below this line
+  
+    // Only change code above this line
+    return arr;
+  }
+  
+  console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
+
+  //ANSWER
+  function htmlColorNames(arr) {
+    // Only change code below this line
+        arr.splice(0,2,'DarkSalmon' , 'BlanchedAlmond');
+    // Only change code above this line
+    return arr;
+  }
+  
+  console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
+
