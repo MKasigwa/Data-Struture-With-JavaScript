69188bac097658d10c622f7555781b3b21624b4c
Miracle Kasigwa <miraclekasigwa@gmail.com>
2021-10-28 08:54:20 +0200

    Added and Solved BDS lecon 9

--- basicDS9.js
+++ basicDS9.js
@@ -21,3 +21,30 @@ Using spread syntax, we have just achieved an operation that
 would have been more complex and more verbose had we used traditional methods.
 */
 
+//QUIZ
+
+/*
+
+We have defined a function spreadOut that returns the variable sentence. 
+Modify the function using the spread operator so that it returns the array 
+['learning', 'to', 'code', 'is', 'fun'].
+
+*/
+
+function spreadOut() {
+    let fragment = ['to', 'code'];
+    let sentence; // Change this line
+    return sentence;
+  }
+  
+  console.log(spreadOut());
+
+
+  //ANSWER
+  function spreadOut() {
+    let fragment = ['to', 'code'];
+    let sentence = ['learning',...fragment,'is','fun']; // Change this line
+    return sentence;
+  }
+  
+  console.log(spreadOut());
