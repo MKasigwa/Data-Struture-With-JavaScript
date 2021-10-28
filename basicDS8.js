66a371a55f7d31908641e6714622dac407cd5b1d
Miracle Kasigwa <miraclekasigwa@gmail.com>
2021-10-28 08:46:29 +0200

    Added and Solved BDS quiz 8

--- basicDS8.js
+++ basicDS8.js
@@ -14,3 +14,42 @@ let thatArray = [...thisArray];
 thatArray equals [true, true, undefined, false, null]. thisArray remains 
 unchanged and thatArray contains the same elements as thisArray.
 */
+
+//QUIZ
+/*
+
+We have defined a function, copyMachine which takes arr (an array) and num 
+(a number) as arguments. The function is supposed to return a new array 
+made up of num copies of arr. We have done most of the work for you,
+ but it doesn't work quite right yet. Modify the function using spread 
+ syntax so that it works correctly (hint: another method we have already 
+    covered might come in handy here!).
+    */
+
+    function copyMachine(arr, num) {
+        let newArr = [];
+        while (num >= 1) {
+          // Only change code below this line
+      
+          // Only change code above this line
+          num--;
+        }
+        return newArr;
+      }
+      
+      console.log(copyMachine([true, false, true], 2));
+
+
+//ANSWER
+function copyMachine(arr, num) {
+    let newArr = [];
+    while (num >= 1) {
+      // Only change code below this line
+          newArr.push([...arr]);
+      // Only change code above this line
+      num--;
+    }
+    return newArr;
+  }
+  
+  console.log(copyMachine([true, false, true], 2));
