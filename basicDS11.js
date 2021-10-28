7c8048cb24ffc7e761522cf49c3408ef93ebdc47
Miracle Kasigwa <miraclekasigwa@gmail.com>
2021-10-28 09:04:47 +0200

    Added quiz 11

--- basicDS11.js
+++ basicDS11.js
@@ -27,3 +27,25 @@ In this way, we have easily and programmatically determined which data
 items are greater than 10, and returned a new array, [12, 14, 80], 
 containing those items.
 */
+
+
+//QUIZ
+/*
+We have defined a function, filteredArray, which takes arr, a nested array, 
+and elem as arguments, and returns a new array. elem represents an element 
+that may or may not be present on one or more of the arrays nested within arr. 
+Modify the function, using a for loop, to return a filtered version of the 
+passed array such that any array nested within arr containing elem has been 
+removed.
+*/
+function filteredArray(arr, elem) {
+    let newArr = [];
+    // Only change code below this line
+  
+    // Only change code above this line
+    return newArr;
+  }
+  
+  console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
+
+  
