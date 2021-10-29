56578ecf2dba99f82ec1a5e83ef2559dd016e367
Miracle Kasigwa <miraclekasigwa@gmail.com>
2021-10-29 15:50:44 +0200

    Added and Solved BDS quiz 12

--- basicDS12.js
+++ basicDS12.js
@@ -36,3 +36,42 @@ nestedArray[2][1][0][0][0] = 'deeper still';
 console.log(nestedArray[2][1][0][0][0]);
 
 //Now it logs deeper still.
+
+//QUIZ
+/*
+We have defined a variable, myNestedArray, set equal
+ to an array. Modify myNestedArray, using any 
+ combination of strings, numbers, and booleans 
+ for data elements, so that it has exactly five 
+ levels of depth (remember, the outer-most array 
+    is level 1). Somewhere on the third level, 
+    include the string deep, on the fourth level, 
+    include the string deeper, and on the fifth level, 
+    include the string deepest.
+    */
+
+    let myNestedArray = [
+        // Only change code below this line
+        ['unshift', false, 1, 2, 3, 'complex', 'nested'],
+        ['loop', 'shift', 6, 7, 1000, 'method'],
+        ['concat', false, true, 'spread', 'array'],
+        ['mutate', 1327.98, 'splice', 'slice', 'push'],
+        ['iterate', 1.3849, 7, '8.4876', 'arbitrary', 'depth']
+        // Only change code above this line
+      ];
+
+
+
+
+      //ANSWER
+
+      let myNestedArray = [
+        // Only change code below this line
+      ["unshift", false, 1, 2, 3, "complex", "nested"],
+        ["loop", "shift", 6, 7, 1000, "method"],
+        ["concat", false, true, "spread", "array", ["deep"]],
+        ["mutate", 1327.98, "splice", "slice", "push", [["deeper"]]],
+        ["iterate", 1.3849, 7, "8.4876", "arbitrary", "depth", [[["deepest"]]]]
+       
+        // Only change code above this line
+      ];
