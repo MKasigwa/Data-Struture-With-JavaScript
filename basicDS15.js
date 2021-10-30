371835f603cf1035c444b1115b392dde03bae6aa
Miracle Kasigwa <miraclekasigwa@gmail.com>
2021-10-30 16:02:56 +0200

    Added and Solved BDS lecon 15

--- basicDS15.js
+++ basicDS15.js
@@ -18,3 +18,45 @@ Bracket notation is very useful because sometimes object properties are not
  known before runtime or we need to access them in a more dynamic way.
  */
 
+//QUIZ
+/*
+We've defined a function, checkInventory, which receives a scanned item as an argument. 
+Return the current value of the scannedItem key in the foods object. You can assume 
+that only valid keys will be provided as an argument to checkInventory.
+*/
+
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27
+};
+
+function checkInventory(scannedItem) {
+  // Only change code below this line
+
+  // Only change code above this line
+}
+
+console.log(checkInventory("apples"));
+
+
+//ANSWER
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28,
+    bananas: 13,
+    grapes: 35,
+    strawberries: 27
+  };
+  
+  function checkInventory(scannedItem) {
+    // Only change code below this line
+  return foods[scannedItem];
+    // Only change code above this line
+  }
+  
+  console.log(checkInventory("apples"));
