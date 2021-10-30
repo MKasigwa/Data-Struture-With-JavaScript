a14f0d68783167312db5494284160358dc685e67
Miracle Kasigwa <miraclekasigwa@gmail.com>
2021-10-30 15:55:52 +0200

    Added and Solved BDS lecon 14

--- basicDS14.js
+++ basicDS14.js
@@ -29,3 +29,43 @@ and data (value is an object with its nested structure). While structures can qu
   */
 
 nestedObject.data.onlineStatus.busy = 10;
+
+
+//QUIZ
+/*
+Here we've defined an object userActivity, which includes another object nested within it. 
+Set the value of the online key to 45.
+*/
+
+let userActivity = {
+    id: 23894201352,
+    date: 'January 1, 2017',
+    data: {
+      totalUsers: 51,
+      online: 42
+    }
+  };
+  
+  // Only change code below this line
+  
+  // Only change code above this line
+  
+  console.log(userActivity);
+
+
+
+  //ANSWER
+  let userActivity = {
+    id: 23894201352,
+    date: 'January 1, 2017',
+    data: {
+      totalUsers: 51,
+      online: 42
+    }
+  };
+  
+  // Only change code below this line
+  userActivity.data.online =45;
+  // Only change code above this line
+  
+  console.log(userActivity);
