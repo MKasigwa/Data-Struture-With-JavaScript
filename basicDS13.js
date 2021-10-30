030138c3af46ef1ba7627f6b47a5793e8f032172
Miracle Kasigwa <miraclekasigwa@gmail.com>
2021-10-30 15:49:17 +0200

    Added and solved BDS lecon 13

--- basicDS13.js
+++ basicDS13.js
@@ -48,3 +48,40 @@ const tekkenCharacter = {
   'hair color': 'dyed orange',
   'eye color': 'brown'
 };
+
+//QUIZ
+/*
+A foods object has been created with three entries. Using the syntax of your choice, 
+add three more entries to it: bananas with a value of 13, grapes with a value of 35, 
+and strawberries with a value of 27.
+*/
+
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28
+  };
+  
+  // Only change code below this line
+  
+  // Only change code above this line
+  
+  console.log(foods);
+
+  //ANSWER
+
+  let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28
+  };
+  
+  // Only change code below this line
+  foods.bananas=13;
+  foods.grapes=35;
+  const straw ='strawberries';
+  foods[straw]=27;
+  
+  // Only change code above this line
+  
+  console.log(foods);
