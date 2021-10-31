b1e4f27a6a1446594cfc39ab4d948cbc0067bbe2
Miracle Kasigwa <miraclekasigwa@gmail.com>
2021-10-31 08:40:04 +0200

    Added BDS lecon 19

---
+++ basicDS19.js
@@ -0,0 +1,8 @@
+//Generate an Array of All Object Keys with Object.keys()
+
+/*
+We can also generate an array which contains all the keys stored in an object using 
+the Object.keys() method and passing in an object as the argument. This will return 
+an array with strings representing each property in the object. Again, there will 
+be no specific order to the entries in the array.
+*/
