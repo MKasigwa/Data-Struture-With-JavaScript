a2689c0696c02d6f7765f7eb28e57a2be75ef2ea
Miracle Kasigwa <miraclekasigwa@gmail.com>
2021-10-31 08:17:53 +0200

    Added and solved BDS quiz 16

--- basicDS16.js
+++ basicDS16.js
@@ -15,3 +15,43 @@ Let's revisit our foods object example one last time. If we wanted to remove the
 */
 
 delete foods.apples;
+
+
+//ANSWER
+
+/*
+Use the delete keyword to remove the oranges, plums, and strawberries keys from the foods
+ object.
+ */
+ let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28,
+    bananas: 13,
+    grapes: 35,
+    strawberries: 27
+  };
+  
+  // Only change code below this line
+  
+  // Only change code above this line
+  
+  console.log(foods);
+
+  //ANSWER
+  let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28,
+    bananas: 13,
+    grapes: 35,
+    strawberries: 27
+  };
+  
+  // Only change code below this line
+  delete foods.oranges;
+  delete foods.plums;
+  delete foods.strawberries;
+  // Only change code above this line
+  
+  console.log(foods);
