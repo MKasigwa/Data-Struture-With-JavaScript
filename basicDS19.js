3dba49ba3792fdace249663dc468a91835d69793
Miracle Kasigwa <miraclekasigwa@gmail.com>
2021-10-31 08:47:01 +0200

    Added and solved BDS lecon 19

--- basicDS19.js
+++ basicDS19.js
@@ -6,3 +6,64 @@ the Object.keys() method and passing in an object as the argument. This will ret
 an array with strings representing each property in the object. Again, there will 
 be no specific order to the entries in the array.
 */
+
+//ANSWER
+/*
+Finish writing the getArrayOfUsers function so that it returns an array containing all 
+the properties in the object it receives as an argument.
+*/
+let users = {
+    Alan: {
+      age: 27,
+      online: false
+    },
+    Jeff: {
+      age: 32,
+      online: true
+    },
+    Sarah: {
+      age: 48,
+      online: false
+    },
+    Ryan: {
+      age: 19,
+      online: true
+    }
+  };
+  
+  function getArrayOfUsers(obj) {
+    // Only change code below this line
+  
+    // Only change code above this line
+  }
+  
+  console.log(getArrayOfUsers(users));
+
+  //ANSWER
+  let users = {
+    Alan: {
+      age: 27,
+      online: false
+    },
+    Jeff: {
+      age: 32,
+      online: true
+    },
+    Sarah: {
+      age: 48,
+      online: false
+    },
+    Ryan: {
+      age: 19,
+      online: true
+    }
+  };
+  
+  function getArrayOfUsers(obj) {
+    // Only change code below this line
+  return Object.keys(obj);
+    // Only change code above this line
+  }
+  
+  console.log(getArrayOfUsers(users));
+
