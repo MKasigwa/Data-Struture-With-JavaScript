9599d15ff9f8bd71e47c1f85a5c33498d272a3fd
Miracle Kasigwa <miraclekasigwa@gmail.com>
2021-10-31 08:23:54 +0200

    Added and Solved QUIZ 17

--- basicDS17.js
+++ basicDS17.js
@@ -11,3 +11,64 @@ users.hasOwnProperty('Alan');
 'Alan' in users;
 
 //Both of these would return true.
+
+//QUIZ
+/*
+Finish writing the function so that it returns true if the object passed to it contains 
+all four names, Alan, Jeff, Sarah and Ryan and returns false otherwise.
+*/
+let users = {
+    Alan: {
+      age: 27,
+      online: true
+    },
+    Jeff: {
+      age: 32,
+      online: true
+    },
+    Sarah: {
+      age: 48,
+      online: true
+    },
+    Ryan: {
+      age: 19,
+      online: true
+    }
+  };
+  
+  function isEveryoneHere(userObj) {
+    // Only change code below this line
+    
+    // Only change code above this line
+  }
+  
+  console.log(isEveryoneHere(users));
+
+  //ANSWER
+  let users = {
+    Alan: {
+      age: 27,
+      online: true
+    },
+    Jeff: {
+      age: 32,
+      online: true
+    },
+    Sarah: {
+      age: 48,
+      online: true
+    },
+    Ryan: {
+      age: 19,
+      online: true
+    }
+  };
+  
+  function isEveryoneHere(userObj) {
+    // Only change code below this line
+    return userObj.hasOwnProperty('Alan') && userObj.hasOwnProperty('Jeff')
+    && userObj.hasOwnProperty('Sarah') && userObj.hasOwnProperty('Ryan')
+    // Only change code above this line
+  }
+  
+  console.log(isEveryoneHere(users));
