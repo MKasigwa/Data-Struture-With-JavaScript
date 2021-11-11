57966c238f3226c354ab6d801bbf0a9436656a5a
Miracle Kasigwa <miraclekasigwa@gmail.com>
2021-11-11 11:15:55 +0200

    Add OOP lecon 4

---
+++ basicAS4.js
@@ -0,0 +1,16 @@
+//Find the Longest Word in a String
+
+/*
+Return the length of the longest word in the provided sentence.
+
+Your response should be a number.
+
+*/
+
+function findLongestWordLength(str) {
+    return str.length;
+  }
+  
+  findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
+  //ANSWER
